eff6fab9c00e238e1077f8d70808206f04a8285e
Nagy-Ahmed-cs <nagya4546@gmail.com>
2024-11-26 00:59:28 +0200

    Starting my programme
    I missed start my project

--- book system/src/Main.java
+++ book system/src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try{
+            first_screen.frist();
+            catch (Exception e){
+            System.out.print(e.getMessage);
+        }
 
 
 
